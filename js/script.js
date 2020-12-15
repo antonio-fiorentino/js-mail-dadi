9e799f5eaf96a073e8ca2d86ef9afbf0c0b21d0b
antonio-fiorentino <antoniofiorentino16@gmail.com>
2020-12-15 16:47:06 +0100

    Esercizio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Banner
+var email = prompt("Benvenuto in Ludopatia, inserire l'email");
+// Verifica email
+if (email != ('ludopatico1@gmail.com')) {
+  alert("E-mail non valida")
+}
+
+// Se il bottone viene schiacciato inizia il gioco
+if(document. getElementById('bottone'). clicked == true) {
+  var min = 1;
+  var max = 6;
+
+  var numeroUtente = Math.floor(Math.random() * (max + 1 - min) + min)
+  var numeroPc = Math.floor(Math.random() * (max + 1 - min) + min)
+
+  console.log(numeroUtente)
+  console.log(numeroPc)
+}
