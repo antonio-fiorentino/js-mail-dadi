a1701ea70f865406b0620048b7eedafc8f71a857
antonio-fiorentino <antoniofiorentino16@gmail.com>
2020-12-16 01:07:31 +0100

    Codice non funzionante

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Banner
 var email = prompt("Benvenuto in Ludopatia, inserire l'email");
 var errore = document.getElementById('errore');
-var btn = document.getElementById('btn');
+var inizio = document.getElementById('inizio');
 var dadopc = document.getElementById('dado-pc');
 var dadoutente = document.getElementById('dado-utente');
 var risultato = document.getElementById('risultato');
 var emailValide =['nonnolibero80@gmail.com' , 'maroon7@gmail.com' , 'pippo@gmail.com'];
-var checkEmail;
+var controlloEmail;
 var min = 1;
 var max = 6;
 var numeroUtente = Math.floor(Math.random() * (max + 1 - min) + min);
@@ -15,20 +15,20 @@ console.log(numeroUtente);
 console.log(numeroPc);
 
 
-var checkEmail = false;
+var controlloEmail = false;
 // Verifica email
 for (var i = 0; i <= emailValide.length; i++) {
   if (email === emailValide[i]) {
-    checkEmail = true;
+    controlloEmail = true;
   }
 }
-if (checkEmail) {
-  btn.innertext ='Clicca qui per incominciare la partita';
-  btn.addEventListener('click',
-    function(){
+// se l'email è corretta iniziarà la partita
+if (controlloEmail) {
+  inizio.innertext ='Clicca qui per incominciare la partita';
+  inizio.addEventListener('click', function(){
 
-      dadopc.innertext = 'PC' + numeroPc;
-      dadoutente.innertext = 'user' + nemeroUtente;
+      dadopc.innertext = 'PC  ' + numeroPc;
+      dadoutente.innertext = 'user  ' + nemeroUtente;
 
       if (numeroUtente > numeroPc) {
         risultato.innertext = 'Bravo hai vinto, la fortuna ti sorride';
@@ -38,7 +38,7 @@ if (checkEmail) {
         risultato.innertext = 'Pareggio, nessuno di noi guadagna';
       }
     })
-
+// Email non corretta
 }else{
   errore.innertext = 'E-mail non corretta';
 }
